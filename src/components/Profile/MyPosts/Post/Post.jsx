3a17e2ac728876b7b2222f082e801f2bdc89000a
grinya0101 <grinya1994015@gmail.com>
2022-10-12 21:02:47 +0300

    add

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import s from './Post.module.css';
 
-type PropsType = {
-    message: string
-    likesCount: number
-}
+const Post = (props) => {
 
-const Post: React.FC<PropsType> = (props) => {
   return (
     <div className={s.item}>
       <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg' />
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-import * as serviceWorker from './serviceWorker';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import SamuraiJSApp from "./App";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<SamuraiJSApp/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-
-// API
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
